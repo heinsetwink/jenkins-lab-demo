87beb0aec69a66b4c44f8ba5f0677d292570923d
Ubuntu <ubuntu@ip-172-31-13-11.ap-southeast-2.compute.internal>
2025-05-01 14:02:59 +0000

    app.py and test_app.py changed

--- test_app.py
+++ test_app.py
@@ -1,16 +1,1 @@
-import unittest
-from app import app  # NOT main
-
-class FlaskAppTestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-        self.app.testing = True
-
-    def test_home(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Hello from Jenkins!', response.data)
-
-if __name__ == '__main__':
-    unittest.main()
-
+from app import main
