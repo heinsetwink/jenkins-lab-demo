d2d013a1b3fac7850edb9b76033e1197a995517a
Ubuntu <ubuntu@ip-172-31-13-11.ap-southeast-2.compute.internal>
2025-05-01 13:42:40 +0000

    Update app.py and test_app.py

--- test_app.py
+++ test_app.py
@@ -1,10 +1,15 @@
-
 import unittest
-from app import main
+from app import app  # NOT main
 
-class TestApp(unittest.TestCase):
-    def test_main(self):
-        self.assertIsNone(main())
+class FlaskAppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Hello from Jenkins!', response.data)
 
 if __name__ == '__main__':
     unittest.main()
